8abdf1597680cd22b3a32492c785d996546b5337
Drexysw <knevdani@gmail.com>
2024-03-25 17:09:12 +0200

    Add project files.

---
+++ OnlineBookstoreManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineBookstoreManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

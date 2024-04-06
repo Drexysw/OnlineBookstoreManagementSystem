e0aee4b5a6bb3723e2dc6da0662b86d9303622a7
Drexysw <knevdani@gmail.com>
2024-04-06 15:02:08 +0300

    Created repository

---
+++ OnlineBookstoreManagementSystem.Infrastructure/Common/Repostitory.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineBookstoreManagementSystem.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineBookstoreManagementSystem.Infrastructure.Common
+{
+    public class Repostitory : IRepository
+    {
+        private readonly ApplicationDbContext context;
+        public Repostitory(ApplicationDbContext _context)
+        {
+            context = _context;
+        }
+        private DbSet<T> DbSet<T>() where T : class 
+        {
+            return this.context.Set<T>();
+        }
+        public IQueryable<T> All<T>() where T : class
+        {
+            return DbSet<T>();
+        }
+
+        public IQueryable<T> AllReadOnly<T>() where T : class
+        {
+            return DbSet<T>()
+                .AsNoTracking();
+        }
+    }
+}
